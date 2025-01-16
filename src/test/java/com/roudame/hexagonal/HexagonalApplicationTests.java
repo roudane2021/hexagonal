a4aa0b8bbe48c6ced9d268f06bd3b90daa3e4047
rachid.roudane <ra.roudane@gmail.com>
2025-01-16 21:31:26 +0100

    first commit

---
+++ src/test/java/com/roudame/hexagonal/HexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roudame.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
